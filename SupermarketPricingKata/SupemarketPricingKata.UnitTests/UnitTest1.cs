fba377315f6b138bc17c035383f34ba39522903a
Aiden <aidenpoulton@gmail.com>
2018-01-12 16:46:58 +0000

    Init

---
+++ SupermarketPricingKata/SupemarketPricingKata.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SupemarketPricingKata.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ SupermarketPricingKata/SupermarketPricingKata.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SupermarketPricingKata.Domain
+{
+    public class Class1
+    {
+    }
+}
